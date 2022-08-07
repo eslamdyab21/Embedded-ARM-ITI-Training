9ce33b0b4a2e501cf1da1a3cb0f3c09f711d4db6
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-07 08:09:07 +0200

    Assignmet-1- add 7segements display functionality and testing

--- Assignmet-1--Traffic-Lightening-system/src/Led_interface.h
+++ Assignmet-1--Traffic-Lightening-system/src/Led_interface.h
@@ -13,4 +13,17 @@
 #define 	RED_LED 		3
 
 
+u8 segmentNumber[10] = {
+        0x3f,  // 0
+        0x06,  // 1
+        0x5b,  // 2
+        0x4f,  // 3
+        0x66,  // 4
+        0x6d,  // 5
+        0x7d,  // 6
+        0x07,  // 7
+        0x7f,  // 8
+        0x67   // 9
+};
+
 #endif /* LED_INTERFACE_H_ */
--- Assignmet-1--Traffic-Lightening-system/src/main.c
+++ Assignmet-1--Traffic-Lightening-system/src/main.c
@@ -20,6 +20,14 @@ void Init(){
       	GPIO_voidSetPinMode(GPIO_PORTA, GREEN_LED, GPIO_PIN_MODE_GP_PP_2MHZ);
       	GPIO_voidSetPinMode(GPIO_PORTA, YELLOW_LED, GPIO_PIN_MODE_GP_PP_2MHZ);
       	GPIO_voidSetPinMode(GPIO_PORTA, RED_LED, GPIO_PIN_MODE_GP_PP_2MHZ);
+
+      	GPIO_voidSetPinMode(GPIO_PORTB, 0, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTB, 1, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTB, 2, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTB, 3, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTB, 4, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTB, 5, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTB, 6, GPIO_PIN_MODE_GP_PP_2MHZ);
 }
 
 void Delay(u16 timeDelay){
@@ -35,6 +43,16 @@ void BlinkLed(u8 Copy_u8Pin, u16 BlinkTime){
 	Delay(BlinkTime);
 }
 
+void SevenSegment_Update(u8 number){
+      GPIO_voidSetPinValue(GPIO_PORTB, 0, ((number>>0)&0x01));
+      GPIO_voidSetPinValue(GPIO_PORTB, 1, ((number>>1)&0x01));
+      GPIO_voidSetPinValue(GPIO_PORTB, 2, ((number>>2)&0x01));
+      GPIO_voidSetPinValue(GPIO_PORTB, 3, ((number>>3)&0x01));
+      GPIO_voidSetPinValue(GPIO_PORTB, 4, ((number>>4)&0x01));
+      GPIO_voidSetPinValue(GPIO_PORTB, 5, ((number>>5)&0x01));
+      GPIO_voidSetPinValue(GPIO_PORTB, 6, ((number>>6)&0x01));
+}
+
 int main(void)
 {
         u16 green_led_blink_time = 10*10000;
@@ -43,9 +61,12 @@ int main(void)
         Init();
 
 	while(1){
+		    SevenSegment_Update(segmentNumber[3]);
+
 		    BlinkLed(GREEN_LED, green_led_blink_time);
 		    BlinkLed(YELLOW_LED, yellow_led_blink_time);
 		    BlinkLed(RED_LED, red_led_blink_time);
+
 	}
 
 
