d794ca3843302d96c6271ab404f2c2a5e2dc75da
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-07 07:08:14 +0200

    Assignmet-1- add 3 leds functionality with the blink time

---
+++ Assignmet-1--Traffic-Lightening-system/src/Led_interface.h
@@ -0,0 +1,16 @@
+/*
+ * Led_interface.h
+ *
+ *  Created on: Aug 7, 2022
+ *      Author: dyab
+ */
+
+#ifndef LED_INTERFACE_H_
+#define LED_INTERFACE_H_
+
+#define		GREEN_LED 		1
+#define 	YELLOW_LED 	2
+#define 	RED_LED 		3
+
+
+#endif /* LED_INTERFACE_H_ */
--- Assignmet-1--Traffic-Lightening-system/src/main.c
+++ Assignmet-1--Traffic-Lightening-system/src/main.c
@@ -9,6 +9,7 @@
 
 #include "GPIO_interface.h"
 #include "RCC_interface.h"
+#include "Led_interface.h"
 
 
 void Init(){
@@ -16,29 +17,37 @@ void Init(){
       	RCC_voidPeripheralClockEnable(RCC_APB2, GPIOA);
 
       	// make pins 1,2,3 output mode
-      	GPIO_voidSetPinMode(GPIO_PORTA, 1, GPIO_PIN_MODE_GP_PP_2MHZ);
-      	GPIO_voidSetPinMode(GPIO_PORTA, 2, GPIO_PIN_MODE_GP_PP_2MHZ);
-      	GPIO_voidSetPinMode(GPIO_PORTA, 3, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTA, GREEN_LED, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTA, YELLOW_LED, GPIO_PIN_MODE_GP_PP_2MHZ);
+      	GPIO_voidSetPinMode(GPIO_PORTA, RED_LED, GPIO_PIN_MODE_GP_PP_2MHZ);
 }
 
-void Delay(int timeDelay){
-      int i=0;
+void Delay(u16 timeDelay){
+      u16 i=0;
       for(i=0; i<timeDelay; i++ );
 
 }
 
+void BlinkLed(u8 Copy_u8Pin, u16 BlinkTime){
+        GPIO_voidSetPinValue(GPIO_PORTA, Copy_u8Pin, GPIO_LOW);
+	Delay(BlinkTime);
+	GPIO_voidSetPinValue(GPIO_PORTA, Copy_u8Pin, GPIO_HIGH);
+	Delay(BlinkTime);
+}
+
 int main(void)
 {
+        u16 green_led_blink_time = 10*10000;
+        u16 yellow_led_blink_time = 3*10000;
+        u16 red_led_blink_time = 10*10000;
         Init();
 
-
 	while(1){
-		    GPIO_voidSetPinValue(GPIO_PORTA, 2, GPIO_HIGH);
-		    Delay(10000);
-		    GPIO_voidSetPinValue(GPIO_PORTA, 2, GPIO_LOW);
-		    Delay(10000);
-
+		    BlinkLed(GREEN_LED, green_led_blink_time);
+		    BlinkLed(YELLOW_LED, yellow_led_blink_time);
+		    BlinkLed(RED_LED, red_led_blink_time);
 	}
+
 
 	return 0;
 }
