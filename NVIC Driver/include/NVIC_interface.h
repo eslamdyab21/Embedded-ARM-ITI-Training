19aef280e882a2e04be3694d1368acfad39303d5
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-09 00:35:41 +0200

    NVIC- add interfacing functions for the interupts registers

--- NVIC Driver/include/NVIC_interface.h
+++ NVIC Driver/include/NVIC_interface.h
@@ -10,4 +10,47 @@
 
 
 
+/*
+ * NVIC_voidEnableInterrupt
+ * parameters: index of interrupt
+ * description: Sets the NVIC enable pin for a specific interrupt
+ */
+void NVIC_voidEnableInterrupt(u8 Copy_u8InterruptPotiotion);
+
+
+/*
+ * NVIC_voidDisableInterrupt
+ * parameters: index of interrupt
+ * description: disable interrupts, and show which interrupts are enabled or disabled
+ */
+void NVIC_voidDisableInterrupt(u8 Copy_u8InterruptPotiotion);
+
+/*
+ * NVIC_voidSetPendingFlag
+ * parameters: index of interrupt
+ * description: force interrupts into the pending state, and show which 
+                interrupts are pending
+ */
+void NVIC_voidSetPendingFlag(u8 Copy_u8InterruptPotiotion);
+
+
+/*
+ * NVIC_voidClearPendingFlag
+ * parameters: index of interrupt
+ * description: remove the pending state from interrupts, and show which
+                interrupts are pending and which are not
+ */
+void NVIC_voidClearPendingFlag(u8 Copy_u8InterruptPotiotion);
+
+/*
+ * NVIC_u8GetActiveFlag
+ * parameters: index of interrupt
+ * return value: 0: not active or 1: active
+ * description: indicate which interrupts are active
+ */
+u8 NVIC_u8GetActiveFlag(u8 Copy_u8InterruptPotiotion);
+
+
+
+
 #endif /* NVIC_INTERFACE_H_ */
