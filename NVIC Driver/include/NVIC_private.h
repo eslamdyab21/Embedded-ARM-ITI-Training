ebb977b6e5eb3b833ea04f33d24317c8c59c4808
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-09 00:10:07 +0200

    NVIC-add ICER0,ICER1 registers

--- NVIC Driver/include/NVIC_private.h
+++ NVIC Driver/include/NVIC_private.h
@@ -12,9 +12,15 @@
 #define NVIC_BASE_ADDRESS  0xE00E100
 
 
-#define NVIC_ISER0	*((volatile u32 *)  (NVIC_BASE_ADDRESS + 0x000) )
+#define NVIC_ISER0  *((volatile u32 *)  (NVIC_BASE_ADDRESS + 0x000) )
 #define NVIC_ISER1	*((volatile u32 *)  (NVIC_BASE_ADDRESS + 0x004) )
-#define NVIC_ISER2	*((volatile u32 *)  (NVIC_BASE_ADDRESS + 0x008) )
+
+
+#define NVIC_ICER0  *((volatile u32 *) (NVIC_BASE_ADDRESS+0x080))
+#define NVIC_ICER1	*((volatile u32 *) (NVIC_BASE_ADDRESS+0x084))
+
+
+
 
 
 
