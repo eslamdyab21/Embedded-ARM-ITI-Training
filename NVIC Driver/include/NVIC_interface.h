f7f9dfef27585445438006dc7679e966f23e8de6
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-15 01:36:53 +0200

    NVIC add macros for the groups and sub-priorities model in interface and config for thr init function

--- NVIC Driver/include/NVIC_interface.h
+++ NVIC Driver/include/NVIC_interface.h
@@ -9,6 +9,17 @@
 #define NVIC_INTERFACE_H_
 
 
+//The IPRx registers provide a 4-bit priority field for each interrupt
+//So we can assign the groups the hole 4 bits (16 groups) leaving no bits for sub-priorities 
+//Or split the 4 bits between the groups and sub-priorities 
+#define		NVIC_GROUP_4_BITS		0       //and 0 bits for sub-priorities (16 group, 0 sub-priorities)
+#define		NVIC_GROUP_3_BITS		1       //and 1 bits for sub-priorities (8 group, 2 sub-priorities)
+#define		NVIC_GROUP_2_BITS		2       //and 2 bits for sub-priorities (4 group, 4 sub-priorities)
+#define		NVIC_GROUP_1_BITS		3       //and 3 bits for sub-priorities (2 group, 8 sub-priorities)
+#define		NVIC_GROUP_0_BITS		4       //and 4 bits for sub-priorities (0 group, 16 sub-priorities)
+
+
+
 /*
  * NVIC_voidInit
  * Parameters: void
