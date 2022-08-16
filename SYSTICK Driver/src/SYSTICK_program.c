21f5fa0dc4bba986f80525bb7d1b085e4da4b65d
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-16 13:44:23 +0200

    SYSTICK- implement STK_voidSetBusyWait function

--- SYSTICK Driver/src/SYSTICK_program.c
+++ SYSTICK Driver/src/SYSTICK_program.c
@@ -22,6 +22,7 @@ void STK_voidInit(u8 Copy_u8ClkSRC){
     switch (Copy_u8ClkSRC)
     {
     case SYSTICK_AHB_8:
+        //CLKSOURCE: Clock source selection (bit-2)
         CLR_BIT(SYSTICK->CTRL,2);
         break;
 
@@ -32,3 +33,48 @@ void STK_voidInit(u8 Copy_u8ClkSRC){
 }
 
 
+/*
+ * STK_voidSetBusyWait
+ * description: Starts a synchronous wait (delay)
+ */
+void STK_voidSetBusyWait(u32 Copy_u32TickCount){
+    /* Disable the systick interrupt
+	 * Set LOAD value
+	 * start the timer
+	 * empty loop until the flag is raised
+	 * reset the flag
+	 * Reset the LOAD register
+	 * stop the timer
+	 */
+
+    //Disable the systick interrupt(incase it was already in use brfore)
+    //TICKINT: SysTick exception request enable
+    //0: Counting down to zero does not assert the SysTick exception request
+    //1: Counting down to zero to asserts the SysTick exception request.
+
+    //we don't need to assert the SysTick exception request because this function
+    //only do busy wait, no functions to call 
+	CLR_BIT(SYSTICK->CTRL, 1);
+
+    //Set LOAD value
+    //Note anded with 0x00FFFFFF because the SysTick can count up to 24-bits only
+	SYSTICK->LOAD = (Copy_u32TickCount - 1) & 0x00FFFFFF;
+
+    //clear the val regitser to start from zero
+	SYSTICK->VAL = 0;
+
+    //start the timer
+	SET_BIT(SYSTICK->CTRL, 0);
+
+    //empty loop until the flag is raised (bit-16 is the COUNT FLAG)
+    //__asm("NOP") is no operation in assembly
+	while(!(GET_BIT(SYSTICK->CTRL, 16))){
+		__asm("NOP");
+	}
+
+    //disable the timer
+	CLR_BIT(SYSTICK->CTRL, 0);
+    
+    //stop the timer
+	SYSTICK->VAL = 0;
+}
