15f30bf6e5bb8370a7bffc2055a87aa84127ef43
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-16 13:11:21 +0200

    SYSTICK- implement STK_voidInit which determaines which clock speed to use based on the value of the 2nd bit in the CTRL register

--- SYSTICK Driver/include/SYSTICK_interface.h
+++ SYSTICK Driver/include/SYSTICK_interface.h
@@ -8,7 +8,18 @@
 #ifndef SYSTICK_INTERFACE_H_
 #define SYSTICK_INTERFACE_H_
 
+//Macros
+#define SYSTICK_AHB_8 		0
+#define SYSTICK_AHB 		1
 
+
+
+
+/*
+ * STK_voidInit
+ * description: Selects the clock source of the SysTick (AHB, AHB/8)
+ */
+void STK_voidInit(u8 Copy_u8ClkSRC);
 
 
 
--- SYSTICK Driver/src/SYSTICK_program.c
+++ SYSTICK Driver/src/SYSTICK_program.c
@@ -13,3 +13,20 @@
 #include "SYSTICK_config.h"
 #include "SYSTICK_private.h"
 
+
+/*
+ * STK_voidInit
+ * description: Selects the clock source of the SysTick (AHB, AHB/8)
+ */
+void STK_voidInit(u8 Copy_u8ClkSRC){
+    switch (Copy_u8ClkSRC)
+    {
+    case SYSTICK_AHB_8:
+        CLR_BIT(SYSTICK->CTRL,2);
+        break;
+
+    case SYSTICK_AHB:
+        SET_BIT(SYSTICK->CTRL,2);
+        break;
+    }
+}
