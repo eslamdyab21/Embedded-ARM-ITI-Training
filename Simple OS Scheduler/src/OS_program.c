4297527b3398a40e0b7d761261e8c8b4ec246aaa
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-16 18:00:55 +0200

    OS- implement OS_voidCreateTask function

--- Simple OS Scheduler/src/OS_program.c
+++ Simple OS Scheduler/src/OS_program.c
@@ -37,3 +37,22 @@ void Scheduler(void){
 	}
 
 }
+
+
+
+/*
+ * OS_voidCreateTask
+ * Paramters:
+ *      Copy_u8ID:           Task unique ID
+ *      Copy_u8Periodicity:  Periodcity of the task
+ *      Copy_u8InitialDelay: Initial delay for the task so that each task can have
+ *                           a time slot of the cpu time
+ *      ptr:                 pointer to a callback function
+ */
+void OS_voidCreateTask(u8 Copy_u8ID, u8 Copy_u8Periodicity, u8 Copy_u8InitialDelay, void (*ptr)(void)){
+    OS_Tasks[Copy_u8ID].Fptr = ptr;
+	OS_Tasks[Copy_u8ID].InitialDelay = Copy_u8InitialDelay;
+	OS_Tasks[Copy_u8ID].Periodicity = Copy_u8Periodicity;
+	OS_Tasks[Copy_u8ID].State = OS_TASK_READY;
+}
+
