ba4a8036ee2bad5595b739bbfea3e6792b0386fd
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-16 17:55:14 +0200

    OS- add functions headers that will be implemented

--- Simple OS Scheduler/include/OS_interface.h
+++ Simple OS Scheduler/include/OS_interface.h
@@ -14,5 +14,46 @@
 #define OS_TASK_SUSPENDED		0
 
 
+/*
+ * OS_voidCreateTask
+ * Paramters:
+ *      Copy_u8ID:           Task unique ID
+ *      Copy_u8Periodicity:  Periodcity of the task
+ *      Copy_u8InitialDelay: Initial delay for the task so that each task can have
+ *                           a time slot of the cpu time
+ *      ptr:                 pointer to a callback function
+ */
+void OS_voidCreateTask(u8 Copy_u8ID, u8 Copy_u8Periodicity, u8 Copy_u8InitialDelay, void (*ptr)(void));
+
+
+/*
+ * OS_voidDeleteTask
+ */
+void OS_voidDeleteTask(u8 Copy_u8ID);
+
+
+/*
+ * OS_voidSuspendTask
+ */
+void OS_voidSuspendTask(u8 Copy_u8ID, u8 Copy_u8SuspendTime);
+
+
+/*
+ * OS_voidStartScheduler
+ */
+void OS_voidStartScheduler(void);
+
+
+/*
+ * OS_voidResumeTask
+ */
+void OS_voidResumeTask(u8 Copy_u8ID);
+
+
+/*
+ * OS_u8GetTaskState
+ */
+u8 OS_u8GetTaskState(u8 Copy_u8ID);
+
 
 #endif /* OS_INTERFACE_H_ */
