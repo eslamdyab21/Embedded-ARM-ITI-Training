c6e052a93da9a20bc34edf96bdbb38c072670d5c
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-19 00:11:45 +0200

    74HC595IC-S2P-7Seg- this one is the same as the one before only differnence is it's a new project in the eye of eclipse

---
+++ 74HC595IC-S2P-7Seg/src/S2P_program.c
@@ -0,0 +1,195 @@
+/*
+ * S2P_program.c
+ *
+ *  Created on: Aug 18, 2022
+ *      Author: dyab
+ */
+
+
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+
+#include "S2P_interface.h"
+#include "S2P_private.h"
+#include "S2P_config.h"
+
+#include "GPIO/GPIO_interface.h"
+#include "RCC Driver/RCC_interface.h"
+
+
+
+/*
+* Function to intialize the pins as output
+*/
+void S2P_voidInit(){
+    RCC_voidInit();
+    switch (S2P_PORT){
+        case PORTA:
+            RCC_voidPeripheralClockEnable(RCC_APB2, GPIOA);
+            GPIO_voidSetPinMode(GPIO_PORTA, S2P_A_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTA, S2P_SHIFT_CLK_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTA, S2P_RESET_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTA, S2P_LATCH_CLK_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTA, S2P_OUTPUT_ENABLE_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+
+            break;
+
+        case PORTB:
+            RCC_voidPeripheralClockEnable(RCC_APB2, GPIOB);
+            GPIO_voidSetPinMode(GPIO_PORTB, S2P_A_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTB, S2P_SHIFT_CLK_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTB, S2P_RESET_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTB, S2P_LATCH_CLK_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTB, S2P_OUTPUT_ENABLE_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+
+            break;
+
+        case PORTC:
+            RCC_voidPeripheralClockEnable(RCC_APB2, GPIOC);
+            GPIO_voidSetPinMode(GPIO_PORTC, S2P_A_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTC, S2P_SHIFT_CLK_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTC, S2P_RESET_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTC, S2P_LATCH_CLK_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            GPIO_voidSetPinMode(GPIO_PORTC, S2P_OUTPUT_ENABLE_PIN, GPIO_PIN_MODE_GP_PP_10MHZ);
+            break;
+
+    }
+
+}
+
+
+/*
+* Function to send serial data
+* Paramters: Copy_u8SerialData 1-bit
+*/
+void S2P_voidTx(u8 Copy_u8SerialData, u8 Copy_u8DigitCount){
+    switch (S2P_PORT){
+        case PORTA:
+            GPIO_voidSetPinValue(GPIO_PORTA, S2P_A_PIN, (Copy_u8SerialData>>Copy_u8DigitCount)&0x01);
+            break;
+
+        case PORTB:
+            GPIO_voidSetPinValue(GPIO_PORTB, S2P_A_PIN, (Copy_u8SerialData>>Copy_u8DigitCount)&0x01);
+            break;
+
+        case PORTC:
+            GPIO_voidSetPinValue(GPIO_PORTC, S2P_A_PIN, (Copy_u8SerialData>>Copy_u8DigitCount)&0x01);
+            break;
+
+    }
+}
+
+
+/*
+* Function to Enable output
+*/
+void S2P_voidOutputEnable(){
+    switch (S2P_PORT){
+        case PORTA:
+            GPIO_voidSetPinValue(GPIO_PORTA, S2P_OUTPUT_ENABLE_PIN, GPIO_LOW);
+            break;
+
+        case PORTB:
+            GPIO_voidSetPinValue(GPIO_PORTB, S2P_OUTPUT_ENABLE_PIN, GPIO_LOW);
+            break;
+
+        case PORTC:
+            GPIO_voidSetPinValue(GPIO_PORTC, S2P_OUTPUT_ENABLE_PIN, GPIO_LOW);
+            break;
+
+    }
+}
+
+
+
+/*
+* Function to Disable output
+*/
+void S2P_voidOutputDisable(){
+    switch (S2P_PORT){
+        case PORTA:
+            GPIO_voidSetPinValue(GPIO_PORTA, S2P_OUTPUT_ENABLE_PIN, GPIO_HIGH);
+            break;
+
+        case PORTB:
+            GPIO_voidSetPinValue(GPIO_PORTB, S2P_OUTPUT_ENABLE_PIN, GPIO_HIGH);
+            break;
+
+        case PORTC:
+            GPIO_voidSetPinValue(GPIO_PORTC, S2P_OUTPUT_ENABLE_PIN, GPIO_HIGH);
+            break;
+
+    }
+}
+
+
+/*
+* Shift Clock Function
+* Paramters: Copy_u8HighOrLow
+*         0: LOW
+*         1: HIGH
+*/
+void S2P_voidShiftClock(u8 Copy_u8HighOrLow){
+    switch (S2P_PORT){
+        case PORTA:
+            GPIO_voidSetPinValue(GPIO_PORTA, S2P_SHIFT_CLK_PIN, Copy_u8HighOrLow);
+            break;
+
+        case PORTB:
+            GPIO_voidSetPinValue(GPIO_PORTB, S2P_SHIFT_CLK_PIN, Copy_u8HighOrLow);
+            break;
+
+        case PORTC:
+            GPIO_voidSetPinValue(GPIO_PORTC, S2P_SHIFT_CLK_PIN, Copy_u8HighOrLow);
+            break;
+
+    }
+}
+
+
+/*
+* Latch Clock Function
+* Paramters: Copy_u8HighOrLow
+*         0: LOW
+*         1: HIGH
+*/
+void S2P_voidLatchClock(u8 Copy_u8HighOrLow){
+    switch (S2P_PORT){
+        case PORTA:
+            GPIO_voidSetPinValue(GPIO_PORTA, S2P_LATCH_CLK_PIN, Copy_u8HighOrLow);
+            break;
+
+        case PORTB:
+            GPIO_voidSetPinValue(GPIO_PORTB, S2P_LATCH_CLK_PIN, Copy_u8HighOrLow);
+            break;
+
+        case PORTC:
+            GPIO_voidSetPinValue(GPIO_PORTC, S2P_LATCH_CLK_PIN, Copy_u8HighOrLow);
+            break;
+
+    }
+}
+
+
+/*
+* Shift Register Reset 
+* Paramters: Copy_u8HighOrLow
+*         0: Reset Disabled
+*         1: Reset Enabled
+*/
+void S2P_voidShiftReset(u8 Copy_u8HighOrLow){
+    switch (S2P_PORT){
+        case PORTA:
+            GPIO_voidSetPinValue(GPIO_PORTA, S2P_RESET_PIN, Copy_u8HighOrLow);
+            break;
+
+        case PORTB:
+            GPIO_voidSetPinValue(GPIO_PORTB, S2P_RESET_PIN, Copy_u8HighOrLow);
+            break;
+
+        case PORTC:
+            GPIO_voidSetPinValue(GPIO_PORTC, S2P_RESET_PIN, Copy_u8HighOrLow);
+            break;
+
+    }
+}
