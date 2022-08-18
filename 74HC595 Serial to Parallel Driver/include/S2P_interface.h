cf3891dfddb42e95d399cbb23b956aaed02cba14
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-18 14:40:10 +0200

    74HC595-S2P- add needed macros for input/output

--- 74HC595 Serial to Parallel Driver/include/S2P_interface.h
+++ 74HC595 Serial to Parallel Driver/include/S2P_interface.h
@@ -8,5 +8,26 @@
 #ifndef S2P_INTERFACE_H_
 #define S2P_INTERFACE_H_
 
+//Macros
+
+//Input
+#define S2P_A                   8       //Serial Data Input
+#define S2P_SHIFT_CLK           8
+#define S2P_RESET               8
+#define S2P_LATCH_CLK           8
+#define S2P_OUTPUT_ENABLE       8
+
+
+
+//Output
+#define S2P_QA  0
+#define S2P_QB  1
+#define S2P_QC  2
+#define S2P_QD  3
+#define S2P_QE  4
+#define S2P_QF  5
+#define S2P_QG  6
+#define S2P_QH  7
+
 
 #endif /* S2P_INTERFACE_H_ */
