52f618fa6bfcc233e88b7c91cffc36a5f0b06ba8
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-18 16:27:14 +0200

    74HC595-S2P- add SysTic and OS drivers

--- 74HC595 Serial to Parallel Driver/src/main.c
+++ 74HC595 Serial to Parallel Driver/src/main.c
@@ -1,5 +1,14 @@
+/*
+ * S2P_program.c
+ *
+ *  Created on: Aug 18, 2022
+ *      Author: dyab
+ */
 
 
+#include "S2P_interface.h"
+#include "SYSTICK Driver/SYSTICK_interface.h"
+#include "Simple OS Scheduler/OS_interface.h"
 
 int main(void){
 
