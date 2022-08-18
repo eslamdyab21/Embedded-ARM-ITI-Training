769ab4bf6370f118c33de38065dc764a4ebd1346
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-18 15:10:41 +0200

    74HC595-S2P- add header functions that will be used in interface

--- 74HC595 Serial to Parallel Driver/include/S2P_interface.h
+++ 74HC595 Serial to Parallel Driver/include/S2P_interface.h
@@ -11,11 +11,11 @@
 //Macros
 
 //Input (PORTB)
-#define S2P_A                   3       //Serial Data Input
-#define S2P_SHIFT_CLK           4
-#define S2P_RESET               5
-#define S2P_LATCH_CLK           6
-#define S2P_OUTPUT_ENABLE       7
+#define S2P_A_PIN                    3       //Serial Data Input
+#define S2P_SHIFT_CLK_PIN            4
+#define S2P_RESET_PIN                5
+#define S2P_LATCH_CLK_PIN            6
+#define S2P_OUTPUT_ENABLE_PIN        7
 
 
 
@@ -28,6 +28,31 @@
 #define S2P_QF  5
 #define S2P_QG  6
 #define S2P_QH  7
+
+
+
+/*
+* Function to set the clock speed for the latch and the shift register
+* Paramters: Copy_u8ShiftClkSpeed and Copy_u8LatchClkSpeed
+*            in ms
+*/
+void S2P_voidInit(u8 Copy_u8ShiftClkSpeed, u8 Copy_u8LatchClkSpeed);
+
+/*
+* Function to send serial data
+* Paramters: Copy_u8SerialData 8-bits
+*/
+void S2P_voidTx(u8 Copy_u8SerialData);
+
+
+/*
+* Function to Enable/Disable output
+* Paramters: Copy_u8Status, 0 for Disable
+*                           1 for Enable
+*/
+void S2P_voidOutputEnaleOrDisable(u8 Copy_u8Status);
+
+
 
 
 #endif /* S2P_INTERFACE_H_ */
