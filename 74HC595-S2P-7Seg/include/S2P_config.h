e09cf41f99d5474b9d1342f1130d7f1b088dfa5b
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-18 18:49:18 +0200

    74HC595-S2P-7Seg- adjust the config pins

--- 74HC595-S2P-7Seg/include/S2P_config.h
+++ 74HC595-S2P-7Seg/include/S2P_config.h
@@ -20,8 +20,8 @@
 //Pins 
 #define S2P_A_PIN                    0       //Serial Data Input
 #define S2P_SHIFT_CLK_PIN            1
-#define S2P_RESET_PIN                2
-#define S2P_LATCH_CLK_PIN            3
+#define S2P_RESET_PIN                3
+#define S2P_LATCH_CLK_PIN            2
 #define S2P_OUTPUT_ENABLE_PIN        4
 
 
