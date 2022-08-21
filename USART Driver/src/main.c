6f394811f28aaafbeface5c3df643071ac2865d3
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-21 09:34:54 +0200

    USART- use the driver in main

--- USART Driver/src/main.c
+++ USART Driver/src/main.c
@@ -16,11 +16,15 @@ int main(void){
     
     //Tx
     u8 bytes[10] = {0,1,2,3,4,5,6,7,8,9};
+    USART_voidTxInt(bytes);
 
+    //Rx
+    u8 RecivedByte = 0;
+    USART_voidRxInt(&RecivedByte);
 
 
     while(1);
 
-    
+
 	return 0;
 }
