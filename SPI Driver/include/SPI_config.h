7fd2ac28800da4d0b0e2d1f7c892436989bb4b3d
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-24 15:40:08 +0200

    SPI- refactore a little bit

--- SPI Driver/include/SPI_config.h
+++ SPI Driver/include/SPI_config.h
@@ -9,15 +9,25 @@
 #define SPI_CONFIG_H_
 
 //Macros
-//Data frmae size
-#define SPI_8BITS    0
-#define SPI_16BITS   1
 
-//Clock Polarity and Cock Phase mode
-#define SPI_CLK_MODE_0   0      //Clock Polarity = 0, Cock Phase = 0
-#define SPI_CLK_MODE_1   1      //Clock Polarity = 0, Cock Phase = 1
-#define SPI_CLK_MODE_2   2      //Clock Polarity = 1, Cock Phase = 0
-#define SPI_CLK_MODE_3   3      //Clock Polarity = 1, Cock Phase = 1
+/*
+Choose a frame size
+    SPI_8BITS
+    SPI_16BITS
+*/
+#define SPI_FRAME_SIZE  SPI_8BITS  
+
+
+/*
+Choose a mode
+    SPI_CLK_MODE_0      //Clock Polarity = 0, Cock Phase = 0
+    SPI_CLK_MODE_1      //Clock Polarity = 0, Cock Phase = 1
+    SPI_CLK_MODE_2      //Clock Polarity = 1, Cock Phase = 0
+    SPI_CLK_MODE_3      //Clock Polarity = 1, Cock Phase = 1
+*/
+#define SPI_CLK_MODE   SPI_CLK_MODE_0
+
+
 
 
 #endif /* SPI_CONFIG_H_ */
--- SPI Driver/include/SPI_interface.h
+++ SPI Driver/include/SPI_interface.h
@@ -9,24 +9,40 @@
 #define SPI_INTERFACE_H_
 
 
+//Macros
+//Data frmae size
+#define SPI_8BITS    0
+#define SPI_16BITS   1
+
+//Clock Polarity and Cock Phase mode
+#define SPI_CLK_MODE_0   0      //Clock Polarity = 0, Cock Phase = 0
+#define SPI_CLK_MODE_1   1      //Clock Polarity = 0, Cock Phase = 1
+#define SPI_CLK_MODE_2   2      //Clock Polarity = 1, Cock Phase = 0
+#define SPI_CLK_MODE_3   3      //Clock Polarity = 1, Cock Phase = 1
+
+
 /*
 * Function to set the data frame size (8/16 bits)
-* Paramters: Copy_u8FramSize :
-                            SPI_8BITS    0
-                            SPI_16BITS   1
+* Paramters :
+        Choose a frame size
+            SPI_8BITS
+            SPI_16BITS
 */
-void SPI_voidDataFrameSize(u8 Copy_u8FramSize);
+void SPI_voidDataFrameSize(void);
 
 
 
 /*
 * Function to configure the Clock Polarity and Cock Phase mode
-* Paramters: Copy_u8FramSize :
-                SPI_CLK_MODE_0      //Clock Polarity = 0, Cock Phase = 0
-                SPI_CLK_MODE_1      //Clock Polarity = 0, Cock Phase = 1
-                SPI_CLK_MODE_2      //Clock Polarity = 1, Cock Phase = 0
-                SPI_CLK_MODE_3      //Clock Polarity = 1, Cock Phase = 1
+* Paramters :
+                
+        Choose a mode in config
+            SPI_CLK_MODE_0      //Clock Polarity = 0, Cock Phase = 0
+            SPI_CLK_MODE_1      //Clock Polarity = 0, Cock Phase = 1
+            SPI_CLK_MODE_2      //Clock Polarity = 1, Cock Phase = 0
+            SPI_CLK_MODE_3      //Clock Polarity = 1, Cock Phase = 1
+
 */
-void SPI_voidClkPolarityPhaseMode(u8 Copy_u8ClkMode);
+void SPI_voidClkPolarityPhaseMode(void);
 
 #endif /* SPI_INTERFACE_H_ */
