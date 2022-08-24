1eef438ad3cff528207e27d242407f07df9cedff
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-24 15:27:25 +0200

    SPI- add two header functions

--- SPI Driver/include/SPI_config.h
+++ SPI Driver/include/SPI_config.h
@@ -8,8 +8,16 @@
 #ifndef SPI_CONFIG_H_
 #define SPI_CONFIG_H_
 
+//Macros
+//Data frmae size
+#define SPI_8BITS    0
+#define SPI_16BITS   1
 
-
+//Clock Polarity and Cock Phase mode
+#define SPI_CLK_MODE_0   0      //Clock Polarity = 0, Cock Phase = 0
+#define SPI_CLK_MODE_1   1      //Clock Polarity = 0, Cock Phase = 1
+#define SPI_CLK_MODE_2   2      //Clock Polarity = 1, Cock Phase = 0
+#define SPI_CLK_MODE_3   3      //Clock Polarity = 1, Cock Phase = 1
 
 
 #endif /* SPI_CONFIG_H_ */
--- SPI Driver/include/SPI_interface.h
+++ SPI Driver/include/SPI_interface.h
@@ -9,7 +9,24 @@
 #define SPI_INTERFACE_H_
 
 
+/*
+* Function to set the data frame size (8/16 bits)
+* Paramters: Copy_u8FramSize :
+                            SPI_8BITS    0
+                            SPI_16BITS   1
+*/
+void SPI_voidDataFrameSize(u8 Copy_u8FramSize);
 
 
+
+/*
+* Function to configure the Clock Polarity and Cock Phase mode
+* Paramters: Copy_u8FramSize :
+                SPI_CLK_MODE_0      //Clock Polarity = 0, Cock Phase = 0
+                SPI_CLK_MODE_1      //Clock Polarity = 0, Cock Phase = 1
+                SPI_CLK_MODE_2      //Clock Polarity = 1, Cock Phase = 0
+                SPI_CLK_MODE_3      //Clock Polarity = 1, Cock Phase = 1
+*/
+void SPI_voidClkPolarityPhaseMode(u8 Copy_u8ClkMode);
 
 #endif /* SPI_INTERFACE_H_ */
