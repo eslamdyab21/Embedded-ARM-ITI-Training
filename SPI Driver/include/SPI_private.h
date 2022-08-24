bac6f65bae29dbdc469e59b49b027c4220aadbae
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-24 14:56:03 +0200

    SPI- add the registers in spi in private.h

--- SPI Driver/include/SPI_private.h
+++ SPI Driver/include/SPI_private.h
@@ -9,6 +9,22 @@
 #define SPI_PRIVATE_H_
 
 
+typedef struct{
+    volatile u32 CR1_REG;
+    volatile u32 CR2_REG;
+    volatile u32 SR_REG;
+    volatile u32 DR_REG;
+    volatile u32 CRCPR_REG;
+    volatile u32 RXCRCR_REG;
+    volatile u32 TXCRCR_REG;
+    volatile u32 I2SCFGR_REG;
+    volatile u32 I2SPR_REG;
+
+}SPI_t;
+
+// SPI1 base address: 0x40013000
+#define SPI1_PERIPHERAL				((volatile SPI_t*) 0x40013000)
+
 
 
 
