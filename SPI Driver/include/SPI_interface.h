1a5b19da14979d6069d33fc16395a973a62a16bf
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-08-25 03:05:07 +0200

    SPI- implement SPI_voidClkPolarityPhaseMode function

--- SPI Driver/include/SPI_interface.h
+++ SPI Driver/include/SPI_interface.h
@@ -17,10 +17,10 @@
 #define SPI_16BITS   1
 
 //Clock Polarity and Cock Phase mode
-#define SPI_CLK_MODE_0   0      //Clock Polarity = 0, Cock Phase = 0
-#define SPI_CLK_MODE_1   1      //Clock Polarity = 0, Cock Phase = 1
-#define SPI_CLK_MODE_2   2      //Clock Polarity = 1, Cock Phase = 0
-#define SPI_CLK_MODE_3   3      //Clock Polarity = 1, Cock Phase = 1
+#define SPI_CLK_MODE_0   0b00      //Clock Polarity = 0, Cock Phase = 0
+#define SPI_CLK_MODE_1   0b01      //Clock Polarity = 0, Cock Phase = 1
+#define SPI_CLK_MODE_2   0b10      //Clock Polarity = 1, Cock Phase = 0
+#define SPI_CLK_MODE_3   0b11      //Clock Polarity = 1, Cock Phase = 1
 
 
 //frame format (MSB-first or LSB-first)
--- SPI Driver/src/SPI_program.c
+++ SPI Driver/src/SPI_program.c
@@ -32,10 +32,39 @@
 void SPI_voidBaudRateClk(void){
     //1.Select the BR[2:0] bits to define the serial clock baud rate 
     //(see SPI_CR1 register)
+    //BR[2:0]: Baud rate control
+
     CLR_BIT(SPI1_PERIPHERAL->CR1_REG,3);
     CLR_BIT(SPI1_PERIPHERAL->CR1_REG,4);
     CLR_BIT(SPI1_PERIPHERAL->CR1_REG,5);
 
     SPI1_PERIPHERAL->CR1_REG |= SPI_BAUD_RATE;
     
+}
+
+
+
+/*
+* Function to configure the Clock Polarity and Cock Phase mode
+* Paramters :
+    SPI_CLK_MODE            
+        Choose a mode in config
+            SPI_CLK_MODE_0      //Clock Polarity = 0, Cock Phase = 0
+            SPI_CLK_MODE_1      //Clock Polarity = 0, Cock Phase = 1
+            SPI_CLK_MODE_2      //Clock Polarity = 1, Cock Phase = 0
+            SPI_CLK_MODE_3      //Clock Polarity = 1, Cock Phase = 1
+
+*/
+void SPI_voidClkPolarityPhaseMode(void){
+    //2.Select the CPOL and CPHA bits to define one of the four relationships 
+    //between the data transfer and the serial clock
+
+    //CPOL: Clock polarity
+    //CPHA: Clock phase
+
+    CLR_BIT(SPI1_PERIPHERAL->CR1_REG,0);
+    CLR_BIT(SPI1_PERIPHERAL->CR1_REG,1);
+
+    SPI1_PERIPHERAL->CR1_REG |= SPI_CLK_MODE;
+
 }
