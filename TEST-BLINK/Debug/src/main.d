ebb4ab108f86b54a445977e4149e98519d45cfc3
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-04 05:04:24 -0700

    remove test blink

--- TEST-BLINK/Debug/src/main.d
+++
@@ -1,8 +0,0 @@
-src/main.o: ../src/main.c ../include/STD_TYPES.h \
- ../include/GPIO_interface.h ../include/RCC_interface.h
-
-../include/STD_TYPES.h:
-
-../include/GPIO_interface.h:
-
-../include/RCC_interface.h:
