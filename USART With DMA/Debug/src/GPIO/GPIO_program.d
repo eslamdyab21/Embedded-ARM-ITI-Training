f41d65936b96d9b20541500c8d819ff3044a469a
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-04 11:18:07 -0700

    USART-DMA- add debug from windows version

---
+++ USART With DMA/Debug/src/GPIO/GPIO_program.d
@@ -0,0 +1,14 @@
+src/GPIO/GPIO_program.o: ../src/GPIO/GPIO_program.c \
+ ../include/STD_TYPES.h ../include/BIT_MATH.h \
+ ../include/GPIO/GPIO_config.h ../include/GPIO/GPIO_interface.h \
+ ../include/GPIO/GPIO_private.h
+
+../include/STD_TYPES.h:
+
+../include/BIT_MATH.h:
+
+../include/GPIO/GPIO_config.h:
+
+../include/GPIO/GPIO_interface.h:
+
+../include/GPIO/GPIO_private.h:
---
+++ USART With DMA/Debug/src/USART Driver/USART_program.d
@@ -0,0 +1,15 @@
+src/USART\ Driver/USART_program.d: ../src/USART\ Driver/USART_program.c \
+ ../include/STD_TYPES.h ../include/BIT_MATH.h \
+ ../include/USART\ Driver/USART_interface.h \
+ ../include/USART\ Driver/USART_config.h \
+ ../include/USART\ Driver/USART_private.h
+
+../include/STD_TYPES.h:
+
+../include/BIT_MATH.h:
+
+../include/USART\ Driver/USART_interface.h:
+
+../include/USART\ Driver/USART_config.h:
+
+../include/USART\ Driver/USART_private.h:
