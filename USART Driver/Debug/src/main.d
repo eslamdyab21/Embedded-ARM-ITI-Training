9f000b2955c1f6bc4ccd24ce6a7d51d7b960223a
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-04 05:14:14 -0700

    USART- proect from windows

---
+++ USART Driver/Debug/src/main.d
@@ -0,0 +1,16 @@
+src/main.o: ../src/main.c ../include/STD_TYPES.h ../include/BIT_MATH.h \
+ ../include/USART_interface.h ../include/RCC\ Driver/RCC_interface.h \
+ ../include/GPIO/GPIO_interface.h \
+ ../include/NVIC\ Driver/NVIC_interface.h
+
+../include/STD_TYPES.h:
+
+../include/BIT_MATH.h:
+
+../include/USART_interface.h:
+
+../include/RCC\ Driver/RCC_interface.h:
+
+../include/GPIO/GPIO_interface.h:
+
+../include/NVIC\ Driver/NVIC_interface.h:
--- USART Driver/system/include/cmsis/cmsis_device.h
+++ USART Driver/system/include/cmsis/cmsis_device.h
@@ -25,32 +25,9 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef __STM32_ASSERT_H
-#define __STM32_ASSERT_H
+#ifndef STM32F1_CMSIS_DEVICE_H_
+#define STM32F1_CMSIS_DEVICE_H_
 
-#ifdef __cplusplus
- extern "C" {
-#endif
+#include "stm32f10x.h"
 
-#if ! defined(assert_param)
-#if defined(USE_FULL_ASSERT)
-/**
-  * @brief  The assert_param macro is used for function's parameters check.
-  * @param  expr: If expr is false, it calls assert_failed function
-  *         which reports the name of the source file and the source
-  *         line number of the call that failed.
-  *         If expr is true, it returns no value.
-  * @retval None
-  */
-  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
-  void assert_failed(uint8_t* file, uint32_t line);
-#else
-  #define assert_param(expr) ((void)0U)
-#endif // USE_FULL_ASSERT
-#endif // assert_param
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // __STM32_ASSERT_H
+#endif // STM32F1_CMSIS_DEVICE_H_
