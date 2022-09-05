d3d8ab5c9df14093381aef6b8d3d3c6119b03384
eslamdyab21 <es-eslam.dyab2019@alexu.edu.eg>
2022-09-05 16:13:11 +0200

    Final-Project-DMA-SPI-UART- adjust RCC_GPIO_NVIC_USART1_SPI1_voidInit

--- Final-Project-DMA-SPI-UART/src/main.c
+++ Final-Project-DMA-SPI-UART/src/main.c
@@ -126,12 +126,13 @@ void DMA_voidInit(u8 Copy_u8ChannelNumber, u32 *Copy_u32SourceAdress, u32 *Copy_
 }
 
 
-void RCC_GPIO_NVIC_voidInit(void){
-    /**************************RCC/GPIO*******************************/
+void RCC_GPIO_NVIC_USART1_SPI1_voidInit(void){
+    /************************RCC/GPIO/USART1/SPI1*********************/
     RCC_voidInit();
 	RCC_voidPeripheralClockEnable(RCC_APB2, GPIOA);
 	RCC_voidPeripheralClockEnable(RCC_APB2, USART1);
 	RCC_voidPeripheralClockEnable(RCC_AHB, DMA1);
+    RCC_voidPeripheralClockEnable(RCC_APB2, SPI1);
 
     GPIO_voidSetPinMode(GPIO_PORTA, 1, GPIO_PIN_MODE_GP_PP_10MHZ);
     GPIO_voidSetPinValue(GPIO_PORTA,1,GPIO_LOW);
@@ -139,10 +140,26 @@ void RCC_GPIO_NVIC_voidInit(void){
     GPIO_voidSetPinMode(GPIO_PORTA, 2, GPIO_PIN_MODE_GP_PP_10MHZ);
     GPIO_voidSetPinValue(GPIO_PORTA,2,GPIO_LOW);
 
+    /*SPI
+    Alternate function      SPI1_REMAP = 0      SPI1_REMAP = 1
+        SPI1_NSS                  PA4               PA15
+        SPI1_SCK                  PA5               PB3
+        SPI1_MISO                 PA6               PB4
+        SPI1_MOSI                 PA7               PB5
+    */    
+    //A7: MOSI (Alternating)
+    GPIO_voidSetPinMode(GPIO_PORTA, 7, GPIO_PIN_MODE_AF_PP_10MHZ);
+    //A5: CLK (Alternating)
+    GPIO_voidSetPinMode(GPIO_PORTA, 5, GPIO_PIN_MODE_AF_PP_10MHZ);
+    //A4: NSS (Alternating)
+    GPIO_voidSetPinMode(GPIO_PORTA, 4, GPIO_PIN_MODE_AF_PP_10MHZ);
+    //A6: MISO (Input)
+    GPIO_voidSetPinMode(GPIO_PORTA, 6, GPIO_PIN_MODE_FLOATING_INPUT);
+
 
     /*********************Enable DMA NVIC-Interupt********************/
     //NVIC_voidInit();
-    /*
+    /*DMA
     11 18 settable DMA1_Channel1    DMA1 Channel1 global interrupt  0x0000_006C
     12 19 settable DMA1_Channel2    DMA1 Channel2 global interrupt  0x0000_0070
     13 20 settable DMA1_Channel3    DMA1 Channel3 global interrupt  0x0000_0074
@@ -152,6 +169,11 @@ void RCC_GPIO_NVIC_voidInit(void){
     17 24 settable DMA1_Channel7    DMA1 Channel7 global interrupt  0x0000_0084
     */
     NVIC_voidEnableInterrupt(11);
+
+    /*********************Enable SPI NVIC-Interupt********************/
+    NVIC_voidInit();
+    //35    42  settable    SPI1    SPI1 global interrupt   0x0000_00CC
+    NVIC_voidEnableInterrupt(35);
     
 }
 
